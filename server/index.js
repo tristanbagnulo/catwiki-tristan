7ddf69e1d0ceb184551edc435fe3c233d4d2a166
Tristan Bagnulo <tristan.bagnulo1@gmail.com>
2023-05-29 18:41:37 +1000

    Implement API calls in Node server.

--- server/index.js
+++ server/index.js
@@ -1,20 +1,50 @@
 const path = require('path');
+const axios = require('axios');
 const express = require("express");
+const cors = require('cors');
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001; 
 
 const app = express();
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(cors());
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from CatWiki!" });
+//Get the list of breed id and name for querying list
+app.get('/breedsList', async (req, res) => {
+  try {
+      const response = await axios.get(`https://api.thecatapi.com/v1/breeds`);
+      return res.json(response.data);
+      
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Error occurred while fetching cat data');
+  }
 });
 
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+//Get the data about a specific cat's image
+// Test URL - http://localhost:3000/catImage?breedId=beng
+// Response - [{"id":"H_UWbOfra","url":"https://cdn2.thecatapi.com/images/H_UWbOfra.jpg","width":1200,"height":1200}]
+app.get('/catImage', async (req, res) => {
+  try {
+      const breedId = req.query.breedId
+      const response = await axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`);
+      res.json(response.data);
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Error occurred while fetching cat data');
+  }
+});
+
+// Get a collection of cat images given the breed
+app.get('/catImages', async (req, res) => {
+  try {
+      const breedId = req.query.breedId
+      const response = await axios.get(`https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${breedId}&api_key=${apiKey}`);
+      res.json(response.data);
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Error occurred while fetching cat data');
+  }
 });
 
 app.listen(PORT, () => {
